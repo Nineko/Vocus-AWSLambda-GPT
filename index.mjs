497d0373f313051a0709ba9f091b8286eb0ec556
Nineko <magacato1532@gmail.com>
2024-05-03 10:08:52 +0800

    add files
    .

---
+++ index.mjs
@@ -0,0 +1,49 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  organization: 'org-RXZzZxZMtjelLN0Kjg0WArUS',
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export const handler = async (event) => {
+  const body = JSON.parse(event.body);
+  const prompt = body.prompt;
+  const params = body.params;
+  console.log(prompt);
+  //console.log(params);
+  if (prompt === undefined || prompt === "") {
+    return {
+      statusCode: 400,
+      bot: "invalid prompt",
+    };
+  }
+  try {
+    const response = await openai.createChatCompletion({
+      model: params.model_name,
+      messages: [{
+        role: "user",
+        content: prompt
+      }],
+      temperature: params.temperature, // Higher values means the model will take more risks.
+      max_tokens: params.max_tokens, // The maximum number of tokens to generate in the completion. 
+      top_p: params.top_p, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: params.frequency_penalty, //decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: params.presence_penalty, // increasing the model's likelihood to talk about new topics.
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({id:body.id,
+        bot:response.data.choices[0].message.content}),
+    };
+  } catch (error) {
+    console.error("API Error:", error.response || error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message || "Something wrong"),
+    };
+  }
+};
